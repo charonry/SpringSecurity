f211b06e66cac2e13b4d8d749d248957147b3502
charon <1178807730@qq.com>
2021-04-07 22:31:36 +0800

    formlogin认证登录

--- basic-server/src/main/java/com/charon/basicserver/config/SecurityConfig.java
+++ basic-server/src/main/java/com/charon/basicserver/config/SecurityConfig.java
@@ -1,8 +1,13 @@
 package com.charon.basicserver.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * @program: SpringSecurity
@@ -15,15 +20,62 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     /**
      * 用于安全认证以及授权规则配置
+     * Controller服务资源要经过一系列的过滤器的验证，我们配置的是验证的放行规则
      * @param http  HttpSecurity
      * @throws Exception
      */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.httpBasic()//开启httpbasic认证
-                .and()
-                .authorizeRequests()
-                .anyRequest()
-                .authenticated();//所有请求都需要登录认证才能访问
+       http.csrf().disable()//禁用跨站csrf攻击防御
+               .formLogin()
+                    .loginPage("/login.html")//一旦用户的请求没有权限就跳转到这个页面\
+                    .loginProcessingUrl("/login")//登录表单form中action的地址，也就是处理认证请求的路径
+                     .usernameParameter("username")//登录表单form中用户名输入框input的name名，不修改的话默认是username
+                    .passwordParameter("password")//form中密码输入框input的name名，不修改的话默认是password
+                     .defaultSuccessUrl("/")//登录认证成功后默认转跳的路径
+               .and()
+               .authorizeRequests()
+                    .antMatchers("/login.html").permitAll()//不需要通过登录验证就可以被访问的资源路径
+                    .antMatchers("/","/biz1","/biz2") //资源路径匹配
+                        .hasAnyAuthority("ROLE_user","ROLE_admin")  //user角色和admin角色都可以访问
+                    .antMatchers("/syslog","/sysuser")  //资源路径匹配
+                        .hasAnyRole("admin")  //admin角色可以访问
+               //.antMatchers("/syslog").hasAuthority("sys:log")
+               //.antMatchers("/sysuser").hasAuthority("sys:user")
+               .anyRequest()
+               .authenticated();
     }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                    .withUser("user")
+                    .password(passwordEncoder().encode("123456"))
+                    .roles("user")
+                .and()
+                    .withUser("admin")
+                    .password(passwordEncoder().encode("123456"))
+                    //.authorities("sys:log","sys:user")
+                    .roles("admin")
+                .and()
+                .passwordEncoder(passwordEncoder());//配置BCrypt加密
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    /**
+     * 开放静态资源权限
+     * 配置的是静态资源的开放，不经过任何的过滤器链验证，直接访问资源。
+     * @param web
+     */
+    @Override
+    public void configure(WebSecurity web) {
+        //将项目中静态资源路径开放出来
+        web.ignoring().antMatchers( "/css/**", "/fonts/**", "/img/**", "/js/**");
+    }
+
+
 }
