eafcef404e443c5f1543f270ea6b22482be7348b
charon <1178807730@qq.com>
2021-04-26 20:28:56 +0800

    认证服务器-密码模式

--- oath2-as/src/main/java/com/charon/oath2as/config/OAuth2AuthorizationServer.java
+++ oath2-as/src/main/java/com/charon/oath2as/config/OAuth2AuthorizationServer.java
@@ -1,10 +1,12 @@
 package com.charon.oath2as.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 
 import javax.annotation.Resource;
@@ -22,6 +24,14 @@ public class OAuth2AuthorizationServer extends AuthorizationServerConfigurerAdap
     @Resource
     PasswordEncoder passwordEncoder;
 
+    @Resource
+    private AuthenticationManager authenticationManager;
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.authenticationManager(authenticationManager);
+    }
+
     @Override
     public void configure(AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
         oauthServer
@@ -29,6 +39,7 @@ public class OAuth2AuthorizationServer extends AuthorizationServerConfigurerAdap
                 .checkTokenAccess("permitAll()")
                 .allowFormAuthenticationForClients();
     }
+
 
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
@@ -39,7 +50,7 @@ public class OAuth2AuthorizationServer extends AuthorizationServerConfigurerAdap
                 // 回调地址就是我们在QQ互联配置的应用回调地址。配置回调地址，选填。
                 .redirectUris("http://localhost:8888/callback")
                 // 授权码模式
-                .authorizedGrantTypes("authorization_code")
+                .authorizedGrantTypes("authorization_code","password")
                 // 可授权的 Scope  Mycode Wm2wzn
                 .scopes("all");
     }
